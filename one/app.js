a9284dd6f6414507d04421ab27531ebf6bf2ac4d
Aakash kumar <aakashkrs001@gmail.com>
2026-01-31 17:42:47 +0530

    react

---
+++ one/app.js
@@ -0,0 +1,20 @@
+
+
+const element=React.createElement('h1',{id:"h1"},"Hello world from React");
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
+
+/*
+<div id:"parent">
+    <div id:"child">
+        <h1>I'm Heading</h1>
+    </div>   
+</div> 
+*/ 
+const el=React.createElement("div",{id:'parent'},
+    React.createElement("div",{id:'child'},
+        React.createElement('h1',{},"I'm Heading")
+    )
+)
+console.log(el);
+root.render(el);
